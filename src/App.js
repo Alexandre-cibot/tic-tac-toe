8bed7fc329b3480dd7fd7b4b5d0a1a9873386fee
Alexandre Cibot <alexandre.cibot@gmail.com>
2020-01-07 14:49:53 +0100

    Feature
    Display winner line

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-function hasAWinner(arr, caseNb) {
+function getWinnerMatrix(arr, caseNb) {
   // Check row
-  if (arr.some(row => row.every(e => !!e && e === row[0]))) {
-    return true;
+  const indexOfWinningRow = arr.findIndex(row =>
+    row.every(e => !!e && e === row[0])
+  );
+  if (indexOfWinningRow > -1) {
+    return arr.map((row, i) => [indexOfWinningRow, i]);
   }
+
   // check column
   if (arr.every(row => row[caseNb] === arr[0][caseNb])) {
-    return true;
+    return arr.map((row, i) => [i, caseNb]);
   }
+
   // check diagonals
   let diagonal1 = [];
   let diagonal2 = [];
@@ -18,16 +23,22 @@ function hasAWinner(arr, caseNb) {
     diagonal2.push(arr[i][arr.length - 1 - i]);
   }
 
-  return (
-    diagonal1.every(e => !!e && e === diagonal1[0]) ||
-    diagonal2.every(e => !!e && e === diagonal2[0])
-  );
+  if (diagonal1.every(e => !!e && e === diagonal1[0])) {
+    return arr.map((row, i) => [i, i]);
+  }
+
+  if (diagonal2.every(e => !!e && e === diagonal2[0])) {
+    return arr.map((row, i) => [i, arr.length - 1 - i]);
+  }
+
+  return [];
 }
 
 function Grid({ n }) {
   const [currentPlayer, setCurrentPlayer] = useState("X");
   const [play, setPlay] = useState(true);
   const [message, setMessage] = useState("");
+  const [winnerMatrix, setWinnerMatrix] = useState([]);
   const [grid, setGrid] = useState(
     new Array(n).fill(1).map(e => new Array(n).fill(null))
   );
@@ -36,10 +47,11 @@ function Grid({ n }) {
     if (play && !event.target.textContent) {
       const newGrid = [...grid];
       newGrid[rowNb][caseNb] = sign;
-      setGrid(newGrid);
-      if (hasAWinner(grid, caseNb)) {
+      let winner = getWinnerMatrix(grid, caseNb);
+      if (winner instanceof Array && winner.length) {
         setMessage(`Player ${currentPlayer} wins`);
         setPlay(false);
+        setWinnerMatrix(winner);
       }
       const nextPlayer = currentPlayer === "X" ? "O" : "X";
       setCurrentPlayer(nextPlayer);
@@ -50,6 +62,7 @@ function Grid({ n }) {
     setPlay(true);
     setGrid(new Array(n).fill(1).map(e => new Array(n).fill(null)));
     setMessage("");
+    setWinnerMatrix([]);
   }
 
   return (
@@ -59,9 +72,13 @@ function Grid({ n }) {
           {grid.map((row, i) => (
             <tr key={`row_${i}`}>
               {row.map((sign, j) => {
+                const isWinnerCell = winnerMatrix.some(
+                  cellPosition => cellPosition[0] === i && cellPosition[1] === j
+                );
                 return (
                   <td
                     key={`cell_${j}`}
+                    style={{ background: isWinnerCell && "springgreen" }}
                     onClick={event => draw(event, i, j, currentPlayer)}
                   >
                     {sign}
